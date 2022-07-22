8c85920e379dc48b045909aa01c43eab6b9ce468
Aris Empanta <eams220891@gmail.com>
2022-07-22 18:44:41 +0300

    building functionality

--- src/components/byIngredient.js
+++ src/components/byIngredient.js
@@ -1,8 +1,35 @@
-
+import axios from "axios"
+import { useState } from "react"
 
 export const ByIngredient = () => {
+    
+    const [cocktailName, setCocktailName] = useState([])
+    const [error, setError] = useState("")
+    
 
+
+    const findIngredient = () => {
+
+      setCocktailName([])
+      setError("")
+
+      let ingredient = document.getElementById("inputIngredient").value    
+     
+
+     axios.get("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient)
+           .then((response) => {                     
+              for(let i=0; i< response.data.drinks.length; i++){        
+                  setCocktailName(oldArray => [...oldArray, response.data.drinks[i].strDrink])                                      
+                }                                   
+           }).catch(() => setError("no such cocktail"))   
+    }
+   
+//
     return(<div>
-             <input type="text" placeholder="Enter ingredient" />
-           </div>)
+             <input type="text" id="inputIngredient" placeholder="Enter ingredient" />
+             <button id="searchIngredient" onClick={ findIngredient }>Search</button>
+             { cocktailName.map(item => <p>{ item}</p>)}
+             <p>{ error }</p>
+           </div>
+          )
 }
