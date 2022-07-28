156dc39b961ba296d33a059c50081252e28cd6cb
Aris Empanta <eams220891@gmail.com>
2022-07-28 17:10:23 +0300

    choosing colors

--- src/components/home.js
+++ src/components/home.js
@@ -7,10 +7,15 @@ export const Home = () => {
 
   return (<div> 
             <div id="homePage">
-              <NavBar />    
-              <div id="homeWrapper">
-                
-              </div>                       
+              <NavBar />  
+                <div id="homeWrapper">
+                  <div id="welcomeWrapper">
+                    <h1 id="welcomeTitle">Cocktail DB</h1>
+                    <p id="welcomeText">An app where you can find any cocktail recipe!</p>
+                  </div>
+                  <img id="homeImage" src={ cocktail } />
+                </div>
+                <div class="wave"></div>                                            
             </div>           
           </div>
     );
